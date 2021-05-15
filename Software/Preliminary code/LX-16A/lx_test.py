6b0057d2423b28561407e8828e82cb9f509d0352
sichunx10 <sichunx10@users.noreply.github.com>
2021-05-15 13:40:36 -0600

    change lx_test for 4 servo motors
    servo ID's are 1, 2, 3, 4
    COM port 8 must be used if code is unchanged

--- Software/Preliminary code/LX-16A/lx_test.py
+++ Software/Preliminary code/LX-16A/lx_test.py
@@ -14,25 +14,57 @@ controller = lewansoul_lx16a.ServoController(
 # or through proxy objects
 servo1 = controller.servo(1)
 servo2 = controller.servo(2)
+servo3 = controller.servo(3)
+servo4 = controller.servo(4)
 
-# servo1.move(100)
+servo4.move(900)
+time.sleep(2)
+
+servo3.move(900)
+time.sleep(2)
+
+servo2.move(900)
+time.sleep(2)
+
+servo1.move(900)
+time.sleep(2)
+
+servo1.move(1)
+time.sleep(1)
+
+servo2.move(1)
+time.sleep(1)
+
+servo3.move(1)
+time.sleep(1)
+
+servo4.move(1)
+time.sleep(1)
 
 # synchronous move of multiple servos
 servo1.move_prepare(900)
 servo2.move_prepare(900)
+servo3.move_prepare(900)
+servo4.move_prepare(900)
 controller.move_start()
 
 time.sleep(2)
 servo1.move_prepare(1)
 servo2.move_prepare(1)
+servo3.move_prepare(1)
+servo4.move_prepare(1)
 controller.move_start()
 
 time.sleep(2)
 servo1.move_prepare(900)
 servo2.move_prepare(900)
+servo3.move_prepare(900)
+servo4.move_prepare(900)
 controller.move_start()
 
 time.sleep(2)
 servo1.move_prepare(1)
 servo2.move_prepare(1)
+servo3.move_prepare(1)
+servo4.move_prepare(1)
 controller.move_start()
